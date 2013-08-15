597573167d2cdc76989d16fb859c4122522b0c4b
Andrian Yablonskyy <andrian.yablonskyy@gmail.com>
2013-08-15 20:58:57 +0300

    Ticket system

--- db/Tickets.cs
+++ db/Tickets.cs
@@ -10,13 +10,14 @@ namespace AY
 		public class Ticket
 		{
 			private Int64 m_clientId = 0;
+			public DateTime m_today;
 			public DateTime m_enter;
 			public DateTime m_leave;
 
 			public Ticket(Int64 id)
 			{	
 				String where = String.Format("clientId = {0}", id);
-				DataRow data = new DbAdapter().GetFirstRow(DbTable.Tickets, where, new List<string> { "enter", "leave" });
+				DataRow data = new DbAdapter().GetFirstRow(DbTable.Tickets, where, new List<string> { "today", "enter", "leave" });
 				
 				if(data == null)
 				{
@@ -25,6 +26,7 @@ namespace AY
 				}
 
 				m_clientId = id;
+				m_today = DateTime.Parse(data["today"].ToString());
 				m_enter = DateTime.Parse(data["enter"].ToString());
 				m_leave = DateTime.Parse(data["leave"].ToString());
 			}
@@ -49,12 +51,44 @@ namespace AY
 					return m_clientId;
 				}
 			}
+
+			public DateTime OpenDate
+			{
+				get
+				{
+					return m_today;
+				}
+				set
+				{
+					DbAdapter da = new DbAdapter();
+					Dictionary<string, string> fields = new Dictionary<string, string>();
+					fields["today"] = value.ToString();
+
+					if (!da.Update(DbTable.Tickets, fields, string.Format("where clientId = {0} ", m_clientId)))
+					{
+						Logger.Error("Error! Ticket creation failed.");
+						throw new Exception("Error! Ticket creation failed.");
+					}
+				}
+			}
 			
 			public DateTime Enter
 			{
 				get
 				{
 					return m_enter;
+				}
+				set
+				{
+					DbAdapter da = new DbAdapter();
+					Dictionary<string, string> fields = new Dictionary<string, string>();
+					fields["enter"] = value.ToString();
+
+					if (!da.Update(DbTable.Tickets, fields, out m_clientId))
+					{
+						Logger.Error("Error! Ticket creation failed.");
+						throw new Exception("Error! Ticket creation failed.");
+					}
 				}
 			}
 
