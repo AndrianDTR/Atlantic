03f4f0b9dd1629b29bd629904e719321a07748f2
andrian.yablonsky <andrian.yablonsky@gmail.com>
2013-12-10 01:47:12 +0200

    Versioning

--- ClientDB/Properties/AssemblyInfo.cs
+++ ClientDB/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("PRO100 Soft")]
 [assembly: AssemblyProduct("E-Assistant")]
-[assembly: AssemblyCopyright("Copyright © Andrian Yablonskyy 2013")]
+[assembly: AssemblyCopyright("Copyright � Andrian Yablonskyy 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.8.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.0.8.1")]
+[assembly: AssemblyFileVersion("0.0.1.1")]
